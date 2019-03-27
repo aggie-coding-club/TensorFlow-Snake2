70a1ef56e3a9c0e5cfecac78496313a5a3d62ac7
Kelvin Wang <30770221+kelvinwop@users.noreply.github.com>
2019-03-27 17:43:03 -0500

    some changes to __init__

--- Part.py
+++ Part.py
@@ -14,14 +14,19 @@ class Part:
     x,y are the row and column values of the part's position
     part_type defines if it is a blank space, snake part, or an apple
     __str__ makes the location and part type string values
+
+    Essentially a struct
     """
-    def __init__(self, x, y, part_type):
+    def __init__(self, x, y, part_type=0):
         """
         description of our constructor
         """
         self.x = x
         self.y = y
         self.part_type = part_type
+
+        # specificly for the snake list
+        self.pnt_next = None
+        self.pnt_prev = None
     def __str__(self):
         return '[%s: (%s,%s)]' % (self.part_type,self.x,self.y)
-    raise NotImplementedError()

5542e73fb3c086792b99913dfa0ec10eef3b7cc4
Kevin Sittser <kevin@sittser.com>
2019-04-08 16:53:42 -0500

    Implemented drop_back()

--- SnakeLL.py
+++ SnakeLL.py
@@ -14,5 +14,16 @@ class SnakeLL:
         self.ll = [obj,self.ll]
         self.length += 1
         self.front = obj
+        if self.length == 1:
+            self.back = obj
     def drop_back(self):
-        raise NotImplementedError()
+        remList = self.ll
+        back = None
+        while remList[1]:
+            back = remList[0]
+            remList = remList[1]
+        self.back = back
+        remList.clear()
+        self.length -= 1
+        if not self.ll:
+            self.front = None

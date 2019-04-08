504d9ce5c3b9e5b21317b952ac606bfab4f74f46
Dylan Theriot <dylantheriot@gmail.com>
2019-04-08 18:53:12 -0500

    unit testing for constructor

---
+++ UnitTests.py
@@ -0,0 +1,18 @@
+''' Unit Tests for methods in SnakeLL '''
+
+from Part import Part
+from SnakeLL import SnakeLL
+
+# Testing constructor
+snake = SnakeLL()
+print("Constructing snake...")
+print("Snake's front is:", snake.front)
+print("Snake's back is:", snake.back)
+print("Snake's length is:", snake.length)
+print("Snake's list of blocks is", snake.ll)
+
+# Testing insert_front
+snake.insert_front(3)
+print(snake.ll)
+
+#Testing drop_back

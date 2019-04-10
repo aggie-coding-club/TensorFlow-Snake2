788c086ef494f81dab06f4b21bd0fca293d10d82
Kelvin Wang <30770221+kelvinwop@users.noreply.github.com>
2019-04-10 16:22:02 -0500

    the game works i promise

--- Game.py
+++ Game.py
@@ -10,6 +10,11 @@ from SnakeLL import SnakeLL
 
 class Game:
     def __init__(self, x, y):
+        """
+
+        :param x: how wide the board is
+        :param y: how high the board is
+        """
         # create variables
         self.x = x
         self.y = y
@@ -24,8 +29,8 @@ class Game:
                 self.board[ix].append(Part(ix, iy))
 
         # place the first snake block at a random location
-        snake_x = numpy.random.randint(0,ix)
-        snake_y = numpy.random.randint(0,iy)
+        snake_x = numpy.random.randint(0, self.x)
+        snake_y = numpy.random.randint(0, self.y)
 
         snake_part = self.board[snake_x][snake_y]
         snake_part.part_type = 1
@@ -33,7 +38,7 @@ class Game:
 
         # create an apple at a random spoooky spot
         blank_spaces = x * y - self.snake.length
-        nth_blank = numpy.random.randint(0,blank_spaces)
+        nth_blank = numpy.random.randint(0,blank_spaces) + 1
         for ix in range(x):
             for iy in range(y):
                 if self.board[ix][iy].part_type == 0:
@@ -43,8 +48,15 @@ class Game:
                     self.apple = self.board[ix][iy]
                     self.apple.part_type = 2
 
+    def __str__(self):
+        return "The board is %sx%s, the snake's head is at: %s, %s" % (self.x,
+                                                                       self.y,
+                                                                       self.snake.front.x,
+                                                                       self.snake.front.y)
 
 
 
 
-#mygame = Game(5,5)
+if __name__ == "__main__":
+    mygame = Game(5, 5)
+    print(mygame)
---
+++ SnakeLLTESTS.py
@@ -0,0 +1,8 @@
+"""
+testing methods in SnakeLL
+"""
+
+from Part import Part
+from SnakeLL import SnakeLL
+
+#do some tests to make sure stuff works properly

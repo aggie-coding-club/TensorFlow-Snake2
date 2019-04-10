4694fb114b2f61d1ab9fedb7163f1c47f7c6acc6
Kelvin Wang <30770221+kelvinwop@users.noreply.github.com>
2019-04-10 16:54:17 -0500

    snake linked list and tests

--- SnakeLL.py
+++ SnakeLL.py
@@ -6,24 +6,43 @@ from Part import Part
 
 class SnakeLL:
     def __init__(self):
-        self.ll = []
         self.front = None
         self.back = None
         self.length = 0
+
     def insert_front(self, obj):
-        self.ll = [obj,self.ll]
+        """
+        insert a Part to the front of the linked list
+        :param obj: a Part
+        :return: nothing
+        """
+        assert isinstance(obj, Part), "Invalid obj type"
+        assert obj.part_type is 1, "You did not insert a snake part"
+        # self.ll = [obj, self.ll]
         self.length += 1
-        self.front = obj
         if self.length == 1:
+            self.front = obj
             self.back = obj
+            obj.pnt_prev = None
+            obj.pnt_next = None
+        else:
+            obj.pnt_next = self.front
+            self.front.pnt_prev = obj
+            self.front = obj
+
     def drop_back(self):
-        remList = self.ll
-        back = None
-        while remList[1]:
-            back = remList[0]
-            remList = remList[1]
-        self.back = back
-        remList.clear()
+        """
+        delete the last Part from the back of the linked list
+        :return: nothing
+        """
+        assert self.length >= 1, "you tried to delete from an empty linked list"
+        # remList = self.ll
+
         self.length -= 1
-        if not self.ll:
+        if self.length > 1:
+            self.back = self.back.pnt_prev
+            self.back.pnt_next.pnt_prev = None
+            self.back.pnt_next = None
+        if self.length is 1:
             self.front = None
+            self.back = None
--- SnakeLLTESTS.py
+++ SnakeLLTESTS.py
@@ -1,8 +1,23 @@
 """
 testing methods in SnakeLL
+
+do some tests to make sure stuff works properly
 """
 
 from Part import Part
 from SnakeLL import SnakeLL
 
-#do some tests to make sure stuff works properly
+if __name__ == "__main__":
+    sll = SnakeLL()
+    assert sll.length is 0, "WRONG LENGTH!!! <initialize>"
+
+    for _ in range(10):  # insert 10 snake parts into a snake linked list
+        part = Part(0, 0, 1)
+        sll.insert_front(part)
+    assert sll.length is 10, "WRONG LENGTH!!! <insert front>"
+
+    for _ in range(10):
+        sll.drop_back()
+    assert sll.length is 0, "WRONG LENGTH!!! <drop back> found: %s" % sll.length
+
+    print("Test passed")
